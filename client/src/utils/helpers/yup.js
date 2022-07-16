319e2daf7e6e7eb0190fe00f5184c34844990480
Max Kemzi <kemzi9585@gmail.com>
2022-07-16 17:20:20 +0300

    Remove useless files

--- client/src/utils/helpers/yup.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const mergeYupSchemas = (...schemas) => {
-	const [first, ...rest] = schemas;
-
-	return rest.reduce(
-		(mergedSchemas, schema) => mergedSchemas.concat(schema),
-		first
-	);
-};
