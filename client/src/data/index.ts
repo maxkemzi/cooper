171235788b47540e6bbcc69a7e77f6979883c246
Max Kemzi <kemzi9585@gmail.com>
2022-07-17 18:40:17 +0300

    Add data interfaces

--- client/src/data/index.ts
+++ client/src/data/index.ts
@@ -1,16 +1,29 @@
-import {IDropdownOption, INavbarItem} from "@customTypes/index";
 import {
 	ABOUT_ME_ROUTE,
 	HOME_ROUTE,
 	PROJECTS_ROUTE
 } from "@utils/constants/routeNames";
 
-export const projectsDropdownItems: IDropdownOption[] = [
+type DataItemId = number;
+
+interface NavbarDataItem {
+	id: DataItemId;
+	text: string;
+	path: string;
+}
+
+interface DropdownOptionDataItem {
+	id: DataItemId;
+	title: string;
+	value: string;
+}
+
+export const projectsDropdownItems: DropdownOptionDataItem[] = [
 	{id: 1, title: "Date", value: "createdDate"},
 	{id: 2, title: "Budget", value: "budget"}
 ];
 
-export const headerNavbarItems: INavbarItem[] = [
+export const headerNavbarItems: NavbarDataItem[] = [
 	{id: 1, text: "Home", path: HOME_ROUTE},
 	{id: 2, text: "Projects", path: PROJECTS_ROUTE},
 	{id: 3, text: "Idea & contact", path: ABOUT_ME_ROUTE}
