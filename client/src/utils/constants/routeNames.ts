4c1972be8164ab93407a7826020f4bc19bee4d58
Max Kemzi <kemzi9585@gmail.com>
2022-07-23 16:11:25 +0300

    Add favorite route name

--- client/src/utils/constants/routeNames.ts
+++ client/src/utils/constants/routeNames.ts
@@ -15,6 +15,7 @@ export const ABOUT_ME_ROUTE: string = "/idea-&-contact";
 export const PROJECTS_ROUTE: string = "/projects";
 export const PROJECT_ROUTE: string = "/projects/:id";
 export const YOUR_PROJECTS_ROUTE: string = "/your-projects";
+export const FAVORITE_PROJECTS_ROUTE: string = "/favorite-projects";
 
 // Profile
 export const PROFILE_ROUTE: string = "/profile";
