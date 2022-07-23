60471bc05b59d5f2be621453b3cbf0f034736cc0
Max Kemzi <kemzi9585@gmail.com>
2022-07-23 16:01:32 +0300

    Add confirm modal component

---
+++ client/src/components/ConfirmModal/ConfirmModal.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const ConfirmModalButtons = styled.div`
+	margin-top: 16px;
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+`;
+
+export default ConfirmModalButtons;
