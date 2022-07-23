9c256c184282672f8fdfdab70724d24214efaeb3
Max Kemzi <kemzi9585@gmail.com>
2022-07-23 16:19:31 +0300

    Add is relative prop to container

--- client/src/components/Container/Container.ts
+++ client/src/components/Container/Container.ts
@@ -1,11 +1,16 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 interface ContainerProps {
 	maxWidth?: string;
+	isRelative?: boolean;
 }
 
 const Container = styled.div<ContainerProps>`
-	position: relative;
+	${({isRelative}) =>
+		isRelative &&
+		css`
+			position: relative;
+		`}
 	max-width: ${({maxWidth}) => maxWidth || "1472px"};
 	padding: 0 16px;
 	margin: 0 auto;
