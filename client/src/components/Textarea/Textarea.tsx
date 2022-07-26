635fdb7f552c131a9c15dc60db96c185339b207a
Max Kemzi <kemzi9585@gmail.com>
2022-07-26 21:11:08 +0300

    Fix textare a onBlur style issue

--- client/src/components/Textarea/Textarea.tsx
+++ client/src/components/Textarea/Textarea.tsx
@@ -1,11 +1,17 @@
-import React, {FC, TextareaHTMLAttributes, useState} from "react";
+import React, {FocusEvent, FC, TextareaHTMLAttributes, useState} from "react";
 import {StyledTextarea, TextareaWrapper} from "./Textarea.styled";
 
-const Textarea: FC<TextareaHTMLAttributes<HTMLTextAreaElement>> = props => {
+const Textarea: FC<TextareaHTMLAttributes<HTMLTextAreaElement>> = ({
+	onBlur,
+	...props
+}) => {
 	const [isFocused, setIsFocused] = useState(false);
 
 	const handleFocus = () => setIsFocused(true);
-	const handleBlur = () => setIsFocused(false);
+	const handleBlur = (e: FocusEvent<HTMLTextAreaElement>) => {
+		onBlur(e);
+		setIsFocused(false);
+	};
 
 	return (
 		<TextareaWrapper isFocused={isFocused}>
