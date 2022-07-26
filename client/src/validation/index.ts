533a4f998b9396bb30aedf2d7d2f1a9b07afa86b
Max Kemzi <kemzi9585@gmail.com>
2022-07-26 21:07:16 +0300

    Complete forms validation

--- client/src/validation/index.ts
+++ client/src/validation/index.ts
@@ -3,30 +3,43 @@ import * as yup from "yup";
 export const createFormValidation = yup.object().shape({
 	title: yup
 		.string()
-		.required("Title is required!")
-		.min(5, "Title minimum value is 5!")
-		.max(25, "Title maximum value is 25!"),
+		.required("Title is required.")
+		.min(5, "Title is too short.")
+		.max(25, "Title is too long."),
 	description: yup
 		.string()
-		.required("Description is required!")
-		.min(10, "Title minimum value is 10!")
-		.max(200, "Title maximum value is 200!"),
+		.required("Description is required.")
+		.min(10, "Description is too short.")
+		.max(400, "Description is too long."),
+	visibility: yup
+		.string()
+		.required("Visibility is required.")
+		.oneOf(["public", "private"]),
 	workType: yup
 		.string()
-		.required("Work type is required!")
-		.oneOf(["default", "remote"])
+		.required("Employment type is required.")
+		.oneOf(["default", "remote"]),
+	budget: yup
+		.number()
+		.min(0)
+		.max(1_000_000, "Maximum budget value is 1,000,000.")
 });
 
 export const contactFormValidation = yup.object().shape({
 	name: yup
 		.string()
 		.required("Name is required.")
-		.max(200, "Name is too long."),
-	email: yup.string().required("Email is required.").email("Invalid email."),
+		.min(3, "Name is too short.")
+		.max(16, "Name is too long."),
+	email: yup
+		.string()
+		.required("Email is required.")
+		.email("Invalid email.")
+		.max(200, "Email is too long."),
 	text: yup
 		.string()
 		.required("Text is required.")
-		.min(5, "Text is too short.")
+		.min(10, "Text is too short.")
 		.max(200, "Text is too long.")
 });
 
@@ -36,7 +49,11 @@ export const signupFormValidation = yup.object().shape({
 		.required("Username is required.")
 		.min(3, "Username is too short.")
 		.max(10, "Username is too long."),
-	email: yup.string().required("Email is required.").email("Invalid email."),
+	email: yup
+		.string()
+		.required("Email is required.")
+		.email("Invalid email.")
+		.max(200, "Email is too long."),
 	password: yup
 		.string()
 		.required("Password is required.")
@@ -49,7 +66,11 @@ export const signupFormValidation = yup.object().shape({
 });
 
 export const emailLoginFormValidation = yup.object().shape({
-	email: yup.string().required("Email is required.").email("Invalid email."),
+	email: yup
+		.string()
+		.required("Email is required.")
+		.email("Invalid email.")
+		.max(200, "Email is too long."),
 	password: yup
 		.string()
 		.required("Password is required.")
@@ -76,8 +97,15 @@ export const editProfileFormValidation = yup.object().shape({
 		.required("Username is required.")
 		.min(3, "Username is too short.")
 		.max(10, "Username is too long."),
-	email: yup.string().required("Email is required.").email("Invalid email."),
-	location: yup.string(),
+	email: yup
+		.string()
+		.required("Email is required.")
+		.email("Invalid email.")
+		.max(200, "Email is too long."),
+	location: yup
+		.string()
+		.min(3, "Location is too short.")
+		.max(30, "Location is too long."),
 	description: yup
 		.string()
 		.min(10, "Description is too short.")
@@ -87,11 +115,24 @@ export const editProfileFormValidation = yup.object().shape({
 export const editProjectFormValidation = yup.object().shape({
 	title: yup
 		.string()
-		.required("Username is required.")
-		.min(3, "Username is too short.")
-		.max(10, "Username is too long."),
+		.required("Title is required.")
+		.min(5, "Title is too short.")
+		.max(25, "Title is too long."),
 	description: yup
 		.string()
+		.required("Description is required.")
 		.min(10, "Description is too short.")
-		.max(200, "Description is too long.")
+		.max(400, "Description is too long."),
+	visibility: yup
+		.string()
+		.required("Visibility is required.")
+		.oneOf(["public", "private"]),
+	workType: yup
+		.string()
+		.required("Employment type is required.")
+		.oneOf(["default", "remote"]),
+	budget: yup
+		.number()
+		.min(0)
+		.max(1_000_000, "Maximum budget value is 1,000,000.")
 });
