4e5060f44cc8e92e5b96ec9d6d7ec42d1f9fe7be
Max Kemzi <kemzi9585@gmail.com>
2022-07-27 14:05:24 +0300

    Remove location field component

--- client/src/components/Form/index.ts
+++ client/src/components/Form/index.ts
@@ -1,14 +1,12 @@
 import FormButton from "./FormButton/FormButton";
 import FormTextField from "./FormTextField/FormTextField";
 import FormTextareaField from "./FormTextareaField/FormTextareaField";
-import FormLocationField from "./FormLocationField/FormLocationField";
 import FormLink from "./FormLink/FormLink";
 import FormFields from "./FormFields/FormFields";
 import Form from "./Form";
 
 export {
 	FormButton,
-	FormLocationField,
 	FormTextField,
 	FormTextareaField,
 	FormLink,
